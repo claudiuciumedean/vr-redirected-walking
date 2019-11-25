43f304c0e863a61c291ff2eb736f3aad7acf8b52
claudiuciumedean <claudiuciumedean@gmail.com>
2019-11-25 15:03:59 +0100

    Start and load scenes completed

--- Assets/scripts/StartScreen.cs
+++ Assets/scripts/StartScreen.cs
@@ -5,17 +5,54 @@ using UnityEngine;
 
 public class StartScreen : MonoBehaviour
 {
-    float timeLeft = 10.0f;
+    public float timeThreshold;
+    public TextMesh text;
+    public GameObject thisObject;
+    float timeLeft;
+    bool enter = false;
+    int answer;
 
 
-    void Update()
-    {
-        timeLeft -= Time.deltaTime;
-        if (timeLeft < 0)
+    private void Update()
+    {   
+        // on controller enter - start countdown
+        if(enter)
         {
-            SceneLoader.Instance.setDistractions(true);
-            SceneLoader.Instance.loadFirstScene();
+            timeLeft -= Time.deltaTime;
+            if (timeLeft < 0)
+            {
+
+                if (thisObject.tag == "Message") // when in start scene
+                {
+                    SceneLoader.Instance.setDistractions(true);
+                    SceneLoader.Instance.loadFirstScene();
+                }
+
+                else // when not in start scene
+                {
+                    
+                    answer = thisObject.tag == "Yes" ? 1 : 0; // pass over the answer to the log, Yes = 1; No = 0
+                    Debug.Log(answer);
+                    // go to next scene from here
+                }
+            }
         }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        timeLeft = timeThreshold;
+        enter = true;
+        text.color = Color.red;
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+        timeLeft = timeThreshold;
+        enter = false;
+        text.color = Color.white;
     }
 
 }
